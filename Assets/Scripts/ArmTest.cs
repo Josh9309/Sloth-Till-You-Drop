e066dcb02cb43356d930670f9116aa0699052dd0
Josh9309 <joshmalmquist96@gmail.com>
2018-01-23 15:16:47 -0500

    experimented with Loopy Arm Mechanic
    -Tried experimenting with using hinge joints for loopy arm mechanic. However it seems that I need to figure out a reliable way to apply a force to the arm in the way that I desire.

---
+++ Assets/Scripts/ArmTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 attractForce = Vector3.MoveTowards(gameObject.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 10 * Time.deltaTime);
+        gameObject.GetComponent<Rigidbody2D>().AddForce(attractForce*1, ForceMode2D.Force);
+	}
+}
---
+++ Assets/Scripts/MouseTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTracker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //set test cursor to be at mouse cursor
+        gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Make cursor follow mouse cursor
+        gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, -5);
+	}
+}
