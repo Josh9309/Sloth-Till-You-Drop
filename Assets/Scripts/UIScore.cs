041b875d50801e4c9c3d25ed638e36d3f0260247
Kevin Idzik <kdi1996@live.com>
2018-01-29 00:44:18 -0500

    Primitive Score UI

---
+++ Assets/Scripts/UIScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Tally up the player's score as they play the game and display it to them
+public class UIScore : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.UI.Text scoreValue; //The score being displayed
+    private Camera gameCamera;
+
+	//Use this for initialization
+	void Start()
+    {
+        gameCamera = FindObjectOfType<Camera>(); //Get the camera, there should only be one in the scene
+	}
+	
+	//Update is called once per frame
+	void Update()
+    {
+        scoreValue.text = ((int)gameCamera.transform.position.y).ToString();  //Calculate the score
+	}
+
+    //TODO: write highscores to file
+}
