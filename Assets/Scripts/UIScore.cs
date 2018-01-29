076fd1580c1845a3642cb6ccf673ef12a0881c09
Kevin Idzik <kdi1996@live.com>
2018-01-29 01:19:47 -0500

    Backend for saving high scores

--- Assets/Scripts/UIScore.cs
+++ Assets/Scripts/UIScore.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO; //Using file input and output
 
 //Tally up the player's score as they play the game and display it to them
 public class UIScore : MonoBehaviour
 {
     [SerializeField] private UnityEngine.UI.Text scoreValue; //The score being displayed
+    private int highscore;
     private Camera gameCamera;
 
 	//Use this for initialization
 	void Start()
     {
         gameCamera = FindObjectOfType<Camera>(); //Get the camera, there should only be one in the scene
-	}
+    }
 	
 	//Update is called once per frame
 	void Update()
@@ -20,5 +22,25 @@ public class UIScore : MonoBehaviour
         scoreValue.text = ((int)gameCamera.transform.position.y).ToString();  //Calculate the score
 	}
 
-    //TODO: write highscores to file
+    //Read the high score from the file
+    void ReadHighScore()
+    {
+        StreamReader fileReader = new StreamReader("Assets\\Data\\highscore.bin"); //Double slash for file path escape sequence
+        string line = null; //Stores the strings being read from the file
+
+        while ((line = fileReader.ReadLine()) != null) //Loop till end of file (which is short in this case)
+        {
+            highscore = int.Parse(line);
+        }
+
+        fileReader.Close(); //Close the StreamReader
+    }
+
+    //Write the high score to the file
+    void WriteHighScore()
+    {
+        StreamWriter fileWriter = new StreamWriter("Assets\\Data\\highscore.bin"); //Double slash for file path escape sequence
+        fileWriter.Write(scoreValue.text);
+        fileWriter.Close(); //Close the StreamWriter
+    }
 }
