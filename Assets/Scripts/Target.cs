c1e39b54924395ed3e7a30c688c2ec0964d48df2
forensic63j <forensic63j@gmail.com>
2018-02-02 00:02:44 -0500

    Started work on procedural target

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,20 +6,52 @@ public class Target : MonoBehaviour {
 
     private Paw sloth;
     private int levelCount;
-    private bool onBranch;
+    private System.Random rand;
+    public GameObject[,] treeGenMatrix;
 
 	// Use this for initialization
 	void Start () {
         sloth = FindObjectOfType<Paw>();
         levelCount = sloth.level;
-        onBranch = false;
+        rand = new System.Random();
+        treeGenMatrix = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<GenerateTree>().treeMatrix;
+        MoveTarget();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(!onBranch)
-        {
-            
-        }
+		
 	}
+
+    //Called by Paw when click/drag succeeds
+    //Adds to level count, moves the target
+    void Climb()
+    {
+        levelCount++;
+        MoveTarget();
+    }
+
+    void MoveTarget()
+    {
+        GameObject branch;
+
+        //Makes it so the target doesn't default to one side
+        int branchCheck = rand.Next(0, 10);
+        if(branchCheck % 2 == 0)
+        {
+            branch = treeGenMatrix[levelCount + 1, 0];
+            if(branch.tag == "Branch")
+            {
+                this.transform.position = new Vector2(-6.65f, branch.transform.position.y);
+            }
+        }
+        else
+        {
+            branch = treeGenMatrix[levelCount + 1, 2];
+            if (branch.tag == "Branch")
+            {
+                this.transform.position = new Vector2(6.65f, branch.transform.position.y);
+            }
+        }
+    }
 }
