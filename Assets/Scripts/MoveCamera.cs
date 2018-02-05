c93629bd76ccfa261fe5d71e684228dbc0b8152f
Kevin Idzik <kdi1996@live.com>
2018-02-05 00:05:46 -0500

    New moving camera

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     private Camera cam;
-    public float speed = 10;
+    public float manualSpeed = 10;
+    public float autoSpeed = .05f;
 
     //Use this for initialization
     void Start()
@@ -17,6 +18,7 @@ public class MoveCamera : MonoBehaviour
 	//Update is called once per frame
 	void Update()
     {
-        cam.transform.position += new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0) * Time.deltaTime;
-	}
+        //cam.transform.position += new Vector3(Input.GetAxis("Horizontal") * manualSpeed, Input.GetAxis("Vertical") * manualSpeed, 0) * Time.deltaTime; //Manual move
+        cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y + autoSpeed, cam.transform.position.z); //Move the camera up
+    }
 }
