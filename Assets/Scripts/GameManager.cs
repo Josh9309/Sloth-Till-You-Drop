667163c8033212a11ca3b25a0cf3b12454f8e84a
Joshua Malmquist (RIT Student) <jxm3392@ad.rit.edu>
2018-02-08 21:53:55 -0500

    Cleaning up Gamestates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState { PAWMODE, LAUNCHMODE};
 public class GameManager : MonoBehaviour {
-
-    public GenerateTree treeGen;
+    public Paw leftPaw;
+    public Paw rightPaw;
+    public Launch slothLaunch;
+    public GameState gameState;
 
 	// Use this for initialization
 	void Start () {
-		
+        leftPaw = GameObject.Find("leftPaw").GetComponent<Paw>();
+        rightPaw = GameObject.Find("rightPaw").GetComponent<Paw>();
+        slothLaunch = GameObject.Find("sloth bod").GetComponent<Launch>();
+        gameState = GameState.PAWMODE;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void SwitchToLaunch()
+    {
+        leftPaw.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+        rightPaw.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+
+        slothLaunch.enabled = true;
+
+        gameState = GameState.LAUNCHMODE;
+    }
+
+    public void SwitchToPaw()
+    {
+        leftPaw.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        rightPaw.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+
+        slothLaunch.enabled = false;
+
+        gameState = GameState.PAWMODE;
+    }
 }
---
+++ Assets/Scripts/Launch.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour {
+
+    private GameManager gameMan;
+    private Rigidbody2D rBody;
+    private CircleCollider2D collider;
+
+    //steering force
+    private Vector2 ultimateForce;
+    private Vector2 velocity;
+    private Vector2 acceleration;
+    public float launchSpeed = 10.0f;
+    public float maxSpeed;
+    public float arrivalSpeed;
+    public float launchWeight = 200.0f;
+
+	// Use this for initialization
+	void Start () {
+        gameMan = GameObject.Find("GameManager").GetComponent<GameManager>();
+        collider = GetComponent<CircleCollider2D>();
+        rBody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 intialMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        intialMousePos.z = 0;
+
+        if(collider.bounds.Contains(intialMousePos))
+            Debug.Log("Mouse in bod");
+
+        if (Input.GetMouseButton(0) && collider.bounds.Contains(intialMousePos))
+        {
+
+            ultimateForce = Vector2.zero;
+
+            //get seek force to mouse
+            ultimateForce += Arrive(intialMousePos, arrivalSpeed) * launchWeight;
+
+            
+            //Limit steering force
+            ultimateForce = Vector2.ClampMagnitude(ultimateForce, maxSpeed);
+
+            //apply acceleration 
+            acceleration = acceleration + (ultimateForce / rBody.mass);
+
+            velocity += acceleration * Time.deltaTime;
+            velocity = Vector2.ClampMagnitude(velocity, launchSpeed);
+
+            rBody.velocity = velocity;
+            acceleration = Vector2.zero;
+            //    Vector3 finalMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            //    Vector2 launchDirection = finalMousePos - intialMousePos;
+
+            //    Vector2 launchForce = launchDirection * launchDirection.magnitude * launchWeight;
+
+            //    rBody.AddForce(launchForce, ForceMode2D.Impulse);
+        }
+	}
+
+    private Vector2 Seek(Vector3 target)
+    {
+        Vector2 seekForce = target - transform.position;
+        seekForce = seekForce.normalized * launchSpeed;
+        seekForce -= velocity;
+        return seekForce;
+    }
+
+    private Vector2 Arrive(Vector3 target, float slowRadius)
+    {
+        Vector2 ArrivalForce = target - transform.position;
+        if (ArrivalForce.magnitude > slowRadius)
+        {
+            //outside arival radius which means no reason to slow down
+            ArrivalForce = ArrivalForce.normalized * launchSpeed;
+        }
+        else
+        {
+            //inside arrival radius, needs to slow down 
+            ArrivalForce = ArrivalForce.normalized * launchSpeed * (ArrivalForce.magnitude / slowRadius);
+        }
+
+        ArrivalForce -= velocity;
+
+        return ArrivalForce;
+    }
+}
