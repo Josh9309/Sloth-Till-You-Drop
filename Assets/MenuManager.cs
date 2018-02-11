dfee534d1118554ada11e66262864fff61911c2a
Joshua Malmquist (RIT Student) <jxm3392@ad.rit.edu>
2018-02-11 15:35:28 -0500

    Created Main Menu scene and added Background
    I made the Main menu Scene for Bruce On Spruce that has a start button to take you to the game. I also when to the unity asset store and found some sky backgrounds to help add scenery to game background

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public Button startBtn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
